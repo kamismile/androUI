411d9f52622b2812c042053f6cd30ed50dafa957
lidong <249288753@qq.com>
2013-12-24 09:36:24 +0800

    00 11

--- androUI/src/main/java/com/examp/androui/button/PageFlipperActivity.java
+++ androUI/src/main/java/com/examp/androui/button/PageFlipperActivity.java
@@ -5,6 +5,7 @@ import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
